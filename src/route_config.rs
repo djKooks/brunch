1b9990c7b92e8dc82eac82baa4b42845704a707e
djKooks <inylove82@gmail.com>
2022-01-16 16:03:48 +0900

    Get route information from file

--- src/route_config.rs
+++ src/route_config.rs
@@ -1,15 +1,51 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fmt::Debug;
+
 use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
 
 pub struct RouteConfig {
     pub route: HashMap<String, String>
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Config {
+    route: Vec<RouteInfo>
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct RouteInfo {
+    from: String,
+    to: String,
+    limit: u16,
+}
+
 impl RouteConfig {
-    pub fn new(_file: &str) -> Self {
-        let mut test_map = HashMap::new();
-        test_map.insert("/v2".to_owned(), "/proxy".to_owned());
-        RouteConfig {
-            route: test_map
+    pub fn new(_file_path: &str) -> Result<Self, ()> {
+
+        match File::open(_file_path) {
+            Ok(file_data) => {
+                let mut buf_reader = BufReader::new(file_data);
+                let mut contents = String::new();
+                
+                buf_reader.read_to_string(&mut contents).unwrap();
+                let config_data: Config = serde_yaml::from_str(&contents).unwrap();
+                // println!("{}, {}, {}",config_data.route[0].from, config_data.route[0].to, config_data.route[0].limit);
+
+                let mut test_map = HashMap::new();
+                for route in config_data.route {
+                    test_map.insert(route.from, route.to);
+                }
+
+                Ok(RouteConfig {
+                    route: test_map
+                })
+            },
+            Err(err) => {
+                panic!("Fail config parsing!")
+            }
         }
     }
 }
